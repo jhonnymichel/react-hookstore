d7882c8539b3e6607754f428ae67ba3481024ee1
Jhonny Michel <jhonnymichel@evolux.net.br>
2019-01-09 21:16:06 -0300

    adding tests to the project

---
+++ src/tests/store.test.js
@@ -0,0 +1,71 @@
+import { createStore, getStore } from '..';
+
+describe('createStore', () => {
+  it('Should create an store and return its public interface', () => {
+    const store = createStore({state: 0, name: '1'});
+    expect(store.getState()).toBe(0);
+    expect(store.name).toBe('1');
+    expect(Object.keys(store)).toEqual(['name', 'setState', 'getState']);
+
+    const store2 = createStore({state: 0});
+    expect(store2.name).toBe('store');
+  });
+
+  it('Should not allow stores with the same name to be created', () => {
+    const store = createStore({state: 0, name: '2'});
+    expect(store).toBeTruthy();
+    expect(() => {
+      createStore({ name: '2' });
+    }).toThrow();
+  });
+});
+
+describe('getStore', () => {
+  it('Should return an store if it exists', () => {
+    createStore({name: 'test'});
+
+    const store = getStore('test');
+    expect(Object.keys(store)).toEqual(['name', 'setState', 'getState']);
+    expect(store.name).toBe('test');
+  })
+
+  it('Should throw an error if store does not exist', () => {
+    expect(() => {
+      getStore('Unexistent store');
+    }).toThrow();
+  });
+});
+
+describe('store', () => {
+  test('store.setState must update store value, store.getState retrieves the value', () => {
+    const store = createStore({state: 0, name: '3'});
+    expect(store.getState()).toBe(0);
+    store.setState(1);
+    expect(store.getState()).toBe(1);
+    store.setState({});
+    expect(store.getState()).toEqual({});
+  });
+
+  test('store.setState must use the provided reducer to handle state update', () => {
+    const reducer = jest.fn();
+    const store = createStore({state: 0, name: '4', reducer});
+    store.setState();
+    store.setState();
+    store.setState();
+    expect(reducer).toHaveBeenCalledTimes(3);
+  });
+
+  test('store.setState must update state according to reducer returned value', () => {
+    const reducer = (state, action) => 5 + action;
+    const store = createStore({state: 0, name: '5', reducer});
+    store.setState(1);
+    expect(store.getState()).toBe(6);
+  });
+
+  test('store.setState must pass previous state and action parameters to reducer', () => {
+    const reducer = jest.fn();
+    const store = createStore({state: 0, name: '6', reducer});
+    store.setState('foo');
+    expect(reducer).toHaveBeenCalledWith(0, 'foo');
+  });
+});
---
+++ src/tests/use-store.test.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { createStore, useStore } from '..';
+
+describe('useStore', () => {
+  const store = createStore({state: 0, name: '1'});
+
+  it('Should throw an error if an inexistent store is referenced', () => {
+    const Component = (props) => {
+      useStore('Unreal Store');
+
+      return <div>Hi</div>;
+    }
+
+    expect(() => {
+      shallow(<Component />);
+    }).toThrow();
+  });
+
+  it('Should provide the component with a functional state and setState pair', () => {
+    const Component = (props) => {
+      const [state, setState] = useStore('1');
+
+      return (
+        <button onClick={() => setState(state+1)}>
+          This button has been clicked {state} times
+        </button>
+      );
+    }
+
+    const rendered = mount(<Component />);
+    expect(rendered.text()).toBe('This button has been clicked 0 times');
+    rendered.find('button').simulate('click');
+    expect(rendered.text()).toBe('This button has been clicked 1 times');
+    rendered.find('button').simulate('click');
+    expect(rendered.text()).toBe('This button has been clicked 2 times');
+  });
+
+  it('Should update all components if setState is called from anywhere', () => {
+    const Component = (props) => {
+      const [state, setState] = useStore('1');
+
+      return (
+        <button onClick={() => setState(state+1)}>
+          This button has been clicked {state} times
+        </button>
+      );
+    }
+
+    const AnotherComponent = (props) => {
+      const [state] = useStore('1');
+
+      return (
+        <div>
+          {state}
+        </div>
+      );
+    }
+
+    store.setState(0);
+
+    const renderedComponent = mount(<Component />);
+    const renderedAnotherComponent = mount(<AnotherComponent />);
+
+    expect(renderedComponent.text()).toBe('This button has been clicked 0 times');
+    expect(renderedAnotherComponent.text()).toBe('0');
+    renderedComponent.find('button').simulate('click');
+    expect(renderedComponent.text()).toBe('This button has been clicked 1 times');
+    expect(renderedAnotherComponent.text()).toBe('1');
+    store.setState('Hello');
+    expect(renderedComponent.text()).toBe('This button has been clicked Hello times');
+    expect(renderedAnotherComponent.text()).toBe('Hello');
+  });
+
+  test('Different stores work in hamorny', () => {
+    const countStore = createStore({name: 'countStore', state: 0});
+    const nameStore = createStore({name: 'nameStore', state: ''});
+
+    const HelloWorld = (props) => {
+      const [name] = useStore('nameStore');
+      const [count] = useStore('countStore');
+
+      return (
+        <div>Hello, {name}! you have been here {count} times!</div>
+      )
+    };
+
+    const rendered = mount(<HelloWorld />);
+    expect(rendered.text()).toBe('Hello, ! you have been here 0 times!');
+
+    countStore.setState(1);
+    expect(rendered.text()).toBe('Hello, ! you have been here 1 times!');
+
+    nameStore.setState('Richard');
+    expect(rendered.text()).toBe('Hello, Richard! you have been here 1 times!');
+  })
+});
