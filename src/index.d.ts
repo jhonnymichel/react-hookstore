fc9c42e9a003fe791ff7e91e2df88cc36c154cba
Paul Irwin <paulirwin@gmail.com>
2019-04-04 11:49:44 -0600

    Fix a few TypeScript typings issues found in testing

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,38 +1,43 @@
-import * as React from "react";
 
-type ReducerType<TState> = (state: TState, payload: any) => TState;
+declare module 'react-hookstore' {
+    type StateCallback<TState> = (state: TState) => void;
 
-type SetStateType<TState> = (state: TState, callback: React.Dispatch<TState>) => void;
+    type ReducerType<TState> = (state: TState, payload: any) => TState;
 
-type DispatchType<TState, TPayload = any> = (payload: TPayload, callback: React.Dispatch<TState>) => void;
+    type SetStateType<TState> = (state: TState, callback?: StateCallback<TState>) => void;
 
-type StoreStateHookType<TState> = [ TState, SetStateType<TState> ];
+    type DispatchType<TState, TPayload = any> = (payload: TPayload, callback?: StateCallback<TState>) => void;
 
-type StoreDispatchHookType<TState, TPayload> = [ TState, DispatchType<TState, TPayload> ];
+    type StoreStateHookType<TState> = [TState, SetStateType<TState>];
 
-declare const defaultReducer: ReducerType<any>;
+    type StoreDispatchHookType<TState> = [TState, DispatchType<TState>];
 
-declare interface StoreSpec<TState> {
-    state: TState;
-    reducer: ReducerType<TState>;
-    setState: SetStateType<TState> | DispatchType<TState>;
-    setters: React.Dispatch<TState>[]
+    const defaultReducer: ReducerType<any>;
+
+    export interface StoreSpec<TState> {
+        state: TState;
+        reducer: ReducerType<TState>;
+        setState: SetStateType<TState> | DispatchType<TState>;
+        setters: StateCallback<TState>[]
+    }
+
+    export interface StateStoreInterface<TState> {
+        setState(state: TState, callback?: StateCallback<TState>): void;
+    }
+
+    export interface ReducerStoreInterface<TState> {
+        dispatch<TPayload>(payload: TPayload, callback?: StateCallback<TState>): void;
+    }
+
+    export function createStore<TState>(name: string, state: TState): StateStoreInterface<TState>;
+
+    export function createStore<TState>(name: string, state: TState, reducer: ReducerType<TState>): ReducerStoreInterface<TState>;
+
+    export function getStoreByName<TState>(name: string): StateStoreInterface<TState> | ReducerStoreInterface<TState>;
+
+    export function useStore<TState>(identifier: string): StoreStateHookType<TState> | StoreDispatchHookType<TState>;
+
+    export function useStore<TState>(store: StateStoreInterface<TState>): StoreStateHookType<TState>;
+
+    export function useStore<TState>(store: ReducerStoreInterface<TState>): StoreDispatchHookType<TState>;
 }
-
-declare class StoreInterface<TState> {
-    constructor(name: string, store: StoreSpec<TState>, useReducer: boolean);
-    setState(state: TState, callback?: React.Dispatch<TState>): void;
-    dispatch(payload: any, callback?: React.Dispatch<TState>): void;    
-}
-
-declare function createStore<TState>(name: string, state?: TState, reducer?: ReducerType<TState>): StoreInterface<TState>;
-
-declare function getStoreByName<TState>(name: string): StoreInterface<TState>; 
-
-declare function useStore<TState, TPayload>(identifier: string): StoreDispatchHookType<TState, TPayload>;
-
-declare function useStore<TState>(identifier: string): StoreStateHookType<TState>;
-
-declare function useStore<TState, TPayload>(store: StoreInterface<TState>): StoreDispatchHookType<TState, TPayload>;
-
-declare function useStore<TState>(store: StoreInterface<TState>): StoreStateHookType<TState>;
